53333550a68b6ef9b7d4175a90929be034309c77
Your Name <you@example.com>
2020-05-25 12:26:19 +0900

    Updates

---
+++ precache-manifest.74634d76d920d01c5045b39584d0f1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e0de3b66705afa173a4855de20de0d",
+    "url": "/watermelon/index.html"
+  },
+  {
+    "revision": "48c6687158b541b2ff09",
+    "url": "/watermelon/static/css/main.6a439f26.chunk.css"
+  },
+  {
+    "revision": "0ea9983e29178bb31934",
+    "url": "/watermelon/static/js/2.2724c19d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/watermelon/static/js/2.2724c19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c6687158b541b2ff09",
+    "url": "/watermelon/static/js/main.f4eba3e1.chunk.js"
+  },
+  {
+    "revision": "f153be25b592b76f9346",
+    "url": "/watermelon/static/js/runtime-main.10c0ed02.js"
+  },
+  {
+    "revision": "17098dbf8e68190ab706ff2cb81f079f",
+    "url": "/watermelon/static/media/watermelon_04.17098dbf.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/watermelon/precache-manifest.74634d76d920d01c5045b39584d0f1e7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/watermelon/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
